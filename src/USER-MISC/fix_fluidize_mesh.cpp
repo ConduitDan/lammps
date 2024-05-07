fa404389922442b024c890e087ffea1821cabcb2
Danny Goldstein <dr.danny.goldstein@gmail.com>
2024-05-07 15:59:55 -0400

    bug fix

--- src/USER-MISC/fix_fluidize_mesh.cpp
+++ src/USER-MISC/fix_fluidize_mesh.cpp
@@ -580,8 +580,8 @@ void FixFluidizeMesh::try_swap(dihedral_type &dihedral) {
   }
   //we've now accepted the flip, time to check the forces on the nodes. We'll just report the force of the bond before and the force of the bond now
   utils::logmesg(lmp, "Accepted A swap: {}-{} -> {}-{}\t",dihedral.atoms[1],dihedral.atoms[2],dihedral.atoms[0],dihedral.atoms[3]);
-  double f0 = calculate_bond_force({dihedral.atoms[1],dihedral.atoms[2]});
-  double f1 = calculate_bond_force({dihedral.atoms[0],dihedral.atoms[3]});
+  double f0 = compute_bond_force({dihedral.atoms[1],dihedral.atoms[2]});
+  double f1 = compute_bond_force({dihedral.atoms[0],dihedral.atoms[3]});
   utils::logmesg(lmp, "Old Bond Force: {}, New bond Force: {}",f0,f1);
 
 
